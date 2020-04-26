3f3f7df7adec7113add0289489ab6ee81d558308
pankona <yosuke.akatsuka@gmail.com>
2020-04-26 23:21:16 +0900

    reduce duplicated codes

--- cmd/cloudfuncs/funcs.go
+++ cmd/cloudfuncs/funcs.go
@@ -11,7 +11,7 @@ import (
 	"google.golang.org/api/compute/v1"
 )
 
-func Start(w http.ResponseWriter, r *http.Request) {
+func withComputeService(f func(ctx context.Context, computeService *compute.Service, g gce.GCE)) {
 	ctx := context.Background()
 	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
 	if err != nil {
@@ -29,63 +29,39 @@ func Start(w http.ResponseWriter, r *http.Request) {
 		Instance: "mario",
 	}
 
-	err = g.DoOperation(ctx, computeService, "start")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("%s\n", err.Error())))
-	}
-	w.WriteHeader(http.StatusOK)
+	f(ctx, computeService, g)
+}
+
+func Start(w http.ResponseWriter, r *http.Request) {
+	withComputeService(func(ctx context.Context, computeService *compute.Service, g gce.GCE) {
+		err := g.DoOperation(ctx, computeService, "start")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("%s\n", err.Error())))
+		}
+		w.WriteHeader(http.StatusOK)
+	})
 }
 
 func Stop(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	computeService, err := compute.New(c)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	g := gce.GCE{
-		Project:  "sponge-is-dry",
-		Zone:     "asia-northeast1-b",
-		Instance: "mario",
-	}
-
-	err = g.DoOperation(ctx, computeService, "stop")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("%s\n", err.Error())))
-	}
-	w.WriteHeader(http.StatusOK)
+	withComputeService(func(ctx context.Context, computeService *compute.Service, g gce.GCE) {
+		err := g.DoOperation(ctx, computeService, "stop")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("%s\n", err.Error())))
+		}
+		w.WriteHeader(http.StatusOK)
+	})
 }
 
 func Status(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	computeService, err := compute.New(c)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	g := gce.GCE{
-		Project:  "sponge-is-dry",
-		Zone:     "asia-northeast1-b",
-		Instance: "mario",
-	}
-
-	status, externalIP, err := g.GetStatus(ctx, computeService)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("%s\n", err.Error())))
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("status: %v, external ip: %v\n", status, externalIP)))
+	withComputeService(func(ctx context.Context, computeService *compute.Service, g gce.GCE) {
+		status, externalIP, err := g.GetStatus(ctx, computeService)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("%s\n", err.Error())))
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("status: %v, external ip: %v\n", status, externalIP)))
+	})
 }
